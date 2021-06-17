4a1f2ac601186cea59d1e9799fb00fd3995cca70
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-17 14:57:05 +0000

    4continued

--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3b0f8c87436a67ff24103e864f95709ddf0e69bafc4ad739731c01f3ec8e42bf319cadcf306cda8e1ed9493820c054ddeffa9bd163fb4d33b3177837b57326ed'
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'homes#top'
+  resources :books, only: [:create,:index,:show,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
 end
