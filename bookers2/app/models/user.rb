6a4eb9993da5af6511b96fc5f7e1dd69eeb10f16
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-03 08:53:07 +0000

    phase2-5continued1

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -19,25 +19,29 @@ class User < ApplicationRecord
 # ↓↓↓↓↓フォロー機能↓↓↓↓↓
 
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローされている側の関係性
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+  # 自分をフォローしているUser
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    # 自分がフォローする側の関係性
   has_many :followings, through: :relationships, source: :followed
-  # 与フォロー関係を通じて参照⇨follower_idをフォローしている人
+    # 自分がフォローしているUser
 
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
+  # createはnewとsaveを合わせた動きをする
 
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
+  # fint_byによって1レコードを見つけ、destroyメソッドで削除をする
 
   def following?(user)
     followings.include?(user)
   end
-
+  # include?によってそのuserをフォローしているか判定している
 
 end
 
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'search/search'
   get 'users/show'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'search#search'
+  
   resources :books, only: [:create,:index,:show,:edit,:update,:destroy] do
     resource :favorites, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
